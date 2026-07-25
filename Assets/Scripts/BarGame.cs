e42ff0ba7bce57f00dc96acc87c27c5bda832e78
Ben Palmer <bencolep@gmail.com>
2026-07-25 19:57:10 -0400

    bar game camera

--- Assets/Scripts/BarGame.cs
+++ Assets/Scripts/BarGame.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BarGame : MonoBehaviour
 {
@@ -9,8 +10,16 @@ public class BarGame : MonoBehaviour
     int direction = 1;
     float max_angle = 30;
     float slider_state = 0f;
-    float slider_bounds = 15f;
+    float slider_bounds = 7.4f;
     int slider_direction = 1;
+
+    //controls
+
+    InputActionMap drinkingControlMap;
+
+    InputAction drink;
+    InputAction leave;
+
 
     Transform slider;
     /** TO DOS:
@@ -48,12 +57,34 @@ public class BarGame : MonoBehaviour
 
     void MoveSlider()
     {
-        float slider_movespeed = slider_direction * 5.3f * Time.deltaTime;
+        float slider_movespeed = (slider_direction * 5.3f + drunkeness) * Time.deltaTime;
 
         slider.Translate(new Vector3(slider_movespeed, 0, 0));
         slider_state += slider_movespeed;
-        if (Math.Abs(slider_state) > slider_bounds) slider_direction = slider_direction * -1;
-        
+        Debug.Log(slider_state);
+
+        if (slider_state > slider_bounds) slider_direction = -1;
+        if (Math.Abs(slider_state) > slider_bounds && slider_state < 0) slider_direction = 1;
 
     }
+
+    void OnDrink()
+    {
+        if(Math.Abs(slider_state) < 1.3f) {
+            drunkeness++;
+            //trigger happiness
+            //play vampire animation
+        } else
+        {
+            LoseDrinking();
+        }
+    }
+
+    void LoseDrinking()
+    {
+        //load bar scene?
+        //play sound effect?
+    }
+
+    
 }
